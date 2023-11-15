9b00d410d7218abf45beebf6bd455313b5ac2917
Bohdan_Havryliuk <bogdangavrilyuk96@gmail.com>
2023-11-15 12:39:18 +0200

    added some stored procedures

---
+++ Shop.Data/Migrations/InitilTable_202311150008.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Migrations
+{
+    internal class InitilTable_202311150008
+    {
+    }
+}
