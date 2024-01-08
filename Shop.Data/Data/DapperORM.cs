09302568be344933753d9bca958b49fb881cd2e5
Serhii Popil <sergii.popil@gmail.com>
2024-01-08 15:54:43 +0200

    fix async/await for DapperORM

--- Shop.Data/Data/DapperORM.cs
+++ Shop.Data/Data/DapperORM.cs
@@ -11,26 +11,26 @@ namespace Shop.Data
         
         public async Task ExecWithoutReturn(string procedureName, object? param)
         {
-             using (var connection = StartConfig._dbContext.CreateRemoteConnection())
+            using (var connection = StartConfig._dbContext.CreateRemoteConnection())
             {
-                connection.Execute(procedureName, param, commandType:CommandType.StoredProcedure);
+                await connection.ExecuteAsync(procedureName, param, commandType:CommandType.StoredProcedure);
             }
         }
         public async Task<T> ExecReturnObject<T>(string procedureName, object? procedureParams)
         {
             using (var connection = StartConfig._dbContext.CreateRemoteConnection())
             {
-                var result = connection.Query<T>(sql: procedureName,
+                var result = await connection.QueryAsync<T>(sql: procedureName,
                     param: procedureParams,
-                    commandType: CommandType.StoredProcedure).FirstOrDefault();
-                return result;
+                    commandType: CommandType.StoredProcedure);
+                return result.FirstOrDefault();
             }
         }
         public async Task<IEnumerable<T>> ExecReturnList<T>(string procedureName, object? param)
         {
             using (var connection = StartConfig._dbContext.CreateRemoteConnection())
             {
-                return connection.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+                return await connection.QueryAsync<T>(procedureName, param, commandType: CommandType.StoredProcedure);
             }
         }
     }
