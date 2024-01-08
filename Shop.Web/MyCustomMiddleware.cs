45d75cdbecab75489bdd8c731fd573dee8af774f
Serhii Popil <sergii.popil@gmail.com>
2024-01-08 15:32:23 +0200

    add custom middleware

---
+++ Shop.Web/MyCustomMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace Shop.Web
+{
+    public class MyCustomMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger _logger;
+        public MyCustomMiddleware(RequestDelegate requestDelegate, ILoggerFactory logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger.CreateLogger("shopLogger");
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.Log(LogLevel.Information, "Start custom middleware");
+            await _requestDelegate.Invoke(context);
+            _logger.Log(LogLevel.Information, "End custom middleware");
+        }
+    }
+}
