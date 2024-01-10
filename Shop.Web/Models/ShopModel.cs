da82d42646812a930ee59d4ece0107a8b901d468
Eugene <eugenemz0908@gmail.com>
2024-01-10 13:35:19 +0200

    Added_Shops_Web_API

---
+++ Shop.Web/Models/ShopModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Web.Models
+{
+    public class ShopModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string? Location { get; set; }
+        public bool IsOpened { get; set; }
+    }
+}
